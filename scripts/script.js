2d3605383a5bda09eca0753d614d4f33ea74fec6
Igwe0001 <nwobodoemmanuel479@gmail.com>
2022-06-06 15:30:07 +0100

    init

---
+++ scripts/script.js
@@ -0,0 +1,39 @@
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector("nav");
+
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabContent = document.querySelectorAll(".tab-content");
+const tabImage = document.querySelectorAll(".page-image");
+
+let toggle = true;
+
+hamburger.addEventListener("click", () => {
+  nav.classList.toggle("show-nav");
+  if (toggle == true) {
+    hamburger.src = "/assets/shared/icon-close.svg";
+  } else {
+    hamburger.src = "/assets/shared/icon-hamburger.svg";
+  }
+  toggle = !toggle;
+});
+
+tabsContainer.addEventListener("click", (e) => {
+  const clicked = e.target.closest(".operations__tab");
+
+  if (!clicked) return;
+
+  tabs.forEach((t) => t.classList.remove("tab-active"));
+    tabContent.forEach((c) => c.classList.remove("tab-active"));
+    tabImage.forEach(e => e.classList.remove('tab-active'))
+
+  clicked.classList.add("tab-active");
+
+  document
+    .querySelector(`.tab-content-${clicked.dataset.tab}`)
+    .classList.add("tab-active");
+
+  document
+    .querySelector(`.page-image-${clicked.dataset.tab}`)
+    .classList.add("tab-active");
+});
